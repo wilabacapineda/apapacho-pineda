86bc0d1f4406deb6d96d6a1e0fff4e5dbed27db2
Walter Pineda Ilabaca <walterio.pineda@gmail.com>
2022-08-10 20:39:03 -0400

    Version 8 Entrega Final 31 de Julio

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -26,11 +26,18 @@ if(enviar_correo){
                 body: formData
             })
             .then(res => {
-                enviando.innerHTML=""
-                enviando.className=""
-                submitted_text.style.display="block"
-                submitted_text.innerHTML="Gracias por escribirnos! su correo será respondido a la brevedad"
-                enviar_correo.style.display="none"                
+                if(res.statusText=="OK" || res.ok == true){
+                    enviando.innerHTML=""
+                    enviando.className=""
+                    submitted_text.style.display="block"
+                    submitted_text.innerHTML="Gracias por escribirnos! su correo será respondido a la brevedad"
+                    enviar_correo.style.display="none"
+                } else {
+                    enviando.innerHTML=""
+                    enviando.className=""
+                    submitted_text.style.display="block"
+                    submitted_text.innerHTML="Error al enviar el correo, por favor intente nuevamente"
+                }
             })
             .catch((error) => {
                 submitted_text.style.display="block"
